7cd343d43fe1a28d47428be353b6ffc75653ac6b
Elena_Kim <dd101282kea@gmail.com>
2021-10-25 23:26:20 +0300

    Анаграмма

--- src/KimElena/hw4/Anagram.java
+++ src/KimElena/hw4/Anagram.java
@@ -1,25 +1,40 @@
 package KimElena.hw4;
 
-/*public class Anagram {
-   /* public static void main(String[] args) {
-        String text1;
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Anagram {
+
+    public static void main(String[] args) {
+        String text1 = "";
+        String text2 = "";
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Введите пожалуйста текст 1");
-
-        text1 = scan.nextLine();
-
-        String text2;
-        Scanner scan2 = new Scanner(System.in);
-
+                text1 = scan.nextLine();
+        String arr1 = removePunct(text1);
         System.out.println("Введите пожалуйста текст 2");
+                text2 = scan.nextLine();
+        String arr2 = removePunct(text2);
 
-        text2 = scan.nextLine();
-
-        String tmp1 = text1;
-
+        Boolean result = arr1.equals(arr2);
+        System.out.println("Являются ли оба текста анаграммой? - " +result );
           }
 
+    public static String removePunct (String str) {
+        StringBuilder result = new StringBuilder(str.length());
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.toUpperCase().charAt(i);
+            if (Character.isAlphabetic(c) || Character.isDigit(c)) {
+                result.append(c);
+            }
+        }
+        char[] chars = result.toString().toCharArray();
+        Arrays.sort(chars);
+        return new String(chars);
     }
-}
-*/
+
+
+    }
+
